b97b7eee1ae456e6923f9395993c98c83cbff848
신준혁 <oridori2705@naver.com>
2023-09-24 16:18:19 +0900

    기본 폴더와 파일 구성 추가
    componets : 여러 컴포넌트에서 공통적으로 사용되는 컴포넌트를 넣어줌
    hooks : 커스텀 훅스들을 넣어줌
    Layout : Footer, Header ,Sidebar 등을 넣어줌
    pages : 각 페이지들인 메인페이지, 업로드페이지 등으 위한 컴포넌트를 넣어줌
    store : 상태관리를 위한 Redux
    utils : 여러 컴포넌트에서 사용되는 함수들을 넣어줌

---
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,8 @@
+//로그인 안된사람이 로그인된 사람만 들어갈 수 있는 경로로 갔을 때 로그인페이지로 이동시키는 Route
+import React from "react";
+
+const ProtectedRoutes = () => {
+  return <div>ProtectedRoutes</div>;
+};
+
+export default ProtectedRoutes;
